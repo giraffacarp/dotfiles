0171104b4ac6a855130603eeddc26d910dc93768
Martin Oswald <73196164+giraffacarp@users.noreply.github.com>
2025-05-13 10:12:54 +0200

    switch to fish shell

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,48 +1,48 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -----------------------------------------------------------
 -- Default prog
 -----------------------------------------------------------
-config.default_prog = { '/opt/homebrew/bin/nu' }
+config.default_prog = { "/opt/homebrew/bin/fish" }
 config.set_environment_variables = {
-    XDG_CONFIG_HOME = os.getenv("HOME") .. "/.config",
+	XDG_CONFIG_HOME = os.getenv("HOME") .. "/.config",
 }
 
 -----------------------------------------------------------
 -- General
 -----------------------------------------------------------
 config.enable_tab_bar = false
-config.window_decorations = 'INTEGRATED_BUTTONS | RESIZE'
+config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
 config.send_composed_key_when_left_alt_is_pressed = true
-config.default_cwd = '~'
+config.default_cwd = "~"
 config.window_padding = {
-  left = '0.5cell',
-  right = '0.5cell',
-  top = '1.5cell',
-  bottom = '0.2cell',
+	left = "0.5cell",
+	right = "0.5cell",
+	top = "1.5cell",
+	bottom = "0.2cell",
 }
 
 -----------------------------------------------------------
 -- Appearance
 -----------------------------------------------------------
-config.color_scheme = 'Catppuccin Macchiato'
-config.font = wezterm.font_with_fallback {
-    {
-        family = 'MonoLisa',
-        stretch = 'Expanded',
-        weight = 'Regular',
-        harfbuzz_features = { 'calt=1', 'liga=1', 'zero', 'ss06', 'ss03', 'ss07' },
-    },
-    {
-        family = 'MonoLisa Nerd Font',
-    }
-}
+config.color_scheme = "Catppuccin Macchiato"
+config.font = wezterm.font_with_fallback({
+	{
+		family = "MonoLisa",
+		stretch = "Expanded",
+		weight = "Regular",
+		harfbuzz_features = { "calt=1", "liga=1", "zero", "ss06", "ss03", "ss07" },
+	},
+	{
+		family = "MonoLisa Nerd Font",
+	},
+})
 config.font_size = 14
 
 -- config
