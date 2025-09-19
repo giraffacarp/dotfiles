80945bd1f9facfa93abbb332b420e26dcc1c84cd
Martin Oswald <73196164+giraffacarp@users.noreply.github.com>
2025-09-19 13:44:56 +0200

    update wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -18,14 +18,12 @@ config.set_environment_variables = {
 -- General
 -----------------------------------------------------------
 config.enable_tab_bar = false
-config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
-config.send_composed_key_when_left_alt_is_pressed = true
 config.default_cwd = "~"
 config.window_padding = {
 	left = "0.5cell",
 	right = "0.5cell",
-	top = "1.5cell",
-	bottom = "0.2cell",
+	top = "0.5cell",
+	bottom = "0.5cell",
 }
 
 -----------------------------------------------------------
@@ -35,9 +33,10 @@ config.color_scheme = "Catppuccin Macchiato"
 config.font = wezterm.font({
 	family = "FiraCode Nerd Font",
 	weight = "Regular",
-	-- harfbuzz_features = { "zero", "ss01", "ss04", "ss05" },
+	harfbuzz_features = { "zero", "ss01", "ss04", "ss05" },
 })
 config.font_size = 15
+config.window_background_opacity = 0.98
 
 -- config
 return config
